17c7c276a56916599fa52ebbeee77fcca4091055
daveleefrags <walkaround@naver.com>
2019-10-06 17:13:27 +0900

    Create [Image]Classification.ipynb

--- [Image]ObjectDetection.py
+++ [Image]ObjectDetection.py
@@ -2,42 +2,53 @@
 
 ### Copy of pytorch tutorial
 ### https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
-
+### 윗 파이토치 튜토리얼의 한글 주석본입니다.
 
 import os
 import numpy as np
-import torch 
-from PIL import Image  
+import torch
+from PIL import Image
 
 
-## Make dataset class 
+## Make dataset class
+## 모델에 활용하기 위한 데이터셋 클래스입니다.
+# 보통 dataset 클래스를 상속받는데 여기서는 그러지 않습니다. 객체인식이라서?...
+
 class PennFudanDataset(object):
     def __init__(self, root, transforms):
-        self.root = root 
-        self.transforms = transforms  
-        
-        # Load all image files, sorting them to 
+        self.root = root
+        self.transforms = transforms
+
+        # Load all image files, sorting them to
         # ensure that they are aligned
+
         # 이미지와 mask 파일의 리스트를 불러옵니다.
+        # 파일을 살펴보면 image에는 말그대로 이미지가, mask에는 검은색 화면만..나옵니다?
+        # 잘 열리지는 않지만 구체적인 instance에 대한 정보를 담고 있는 듯 합니다.
         self.imgs = list(sorted(os.path.join(root, "PNGImages")))
         self.masks = list(sorted(os.path.join(root, "PedMasks")))
-        
+
+    # 인덱스에 해당하는 인풋과 타겟 정보를 출력하기 위한 펑션
     def __getitem__(self, idx):
 
+        # 해당 인덱스에 해당하는 이미지와 마스크 정보의 경로를 가져오는 부분
         img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
         mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
 
-        # Note that we haven't converted the mask to RGB, 
+        # Note that we haven't converted the mask to RGB,
         # because each color corresponds to  different instance
         # with 0 being background
-        # mask는 별도로 RGB로 컨버팅하지 않습니다. (3개의 채널로 이루어져 있지 않음)
+        # (mask는 별도로 RGB로 컨버팅하지 않습니다. (3개의 채널로 이루어져 있지 않음) )
+
+        # 이미지는 연 뒤에 rgb로 컨버팅하고
+        # 마스크는 그냥 가지고 와서 np.array로 변형함
         img = Image.open(img_path).convert("RGB")
         mask = Image.open(img_path)
-        mask = np.array(mask)  
+        mask = np.array(mask)
 
         # mask에 있는 유니크한 값들을 가지고 옴
         # 첫번째 값은 0 - 즉 백그라운드이기 때문에 - 버리고 두번째부터 가지고 옴
-        obj_ids = np.unique(mask) 
+        obj_ids = np.unique(mask)
         obj_ids = obj_ids[1:]
 
         # Split the color-encoded coodinates for each mask of binary masks
@@ -45,41 +56,41 @@ class PennFudanDataset(object):
         masks = mask == obj_ids[:, None, None]
 
         # get bounding box coordinates for each mask
-        num_objs = len(obj_ids) 
-        boxes = [] 
+        num_objs = len(obj_ids)
+        boxes = []
         for i in range(num_objs):
             pos = np.where(masks[i])
             xmin = np.min(pos[1])
             xmax = np.max(pos[1])
             ymin = np.min(pos[0])
             ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax]) 
+            boxes.append([xmin, ymin, xmax, ymax])
 
         # convert everything into a torch.Tensor
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        labels = torch.ones((num_objs,), dtype=torch.int64) 
+        labels = torch.ones((num_objs,), dtype=torch.int64)
         masks = torch.as_tensor(masks, dtype=torch.uint8)
 
         image_id = torch.tensor([idx])
-        area = (boxes[:,3] - boxes[:, 1]) * (boxes[:,2] - boxes[:,0]) 
-        
+        area = (boxes[:,3] - boxes[:, 1]) * (boxes[:,2] - boxes[:,0])
+
         iscrowded = torch.zeros((num_objs,), dtype=torch.int64)
 
-        target = {} 
-        target['boxes'] = boxes 
-        target['labels'] = labels  
-        target['masks'] = masks   
-        target['image_id'] = image_id  
-        target['area'] = area 
-        target['iscrowd'] = iscrowd  
+        target = {}
+        target['boxes'] = boxes
+        target['labels'] = labels
+        target['masks'] = masks
+        target['image_id'] = image_id
+        target['area'] = area
+        target['iscrowd'] = iscrowd
 
         if self.transforms is not None:
-            img, target = self.transforms(img, target) 
+            img, target = self.transforms(img, target)
 
-        return img, target  
+        return img, target
 
     def __len__(self):
-        return len(self.imgs) 
+        return len(self.imgs)
 
 
 
@@ -89,14 +100,14 @@ class PennFudanDataset(object):
 # Option 1 :finetunning from a pretrained model
 # pretrained된 모델을 불러와서 tunning 하는 방식입니다.
 
-import torchvision 
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor 
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
 num_classes = 2 # 1 class (person) + background (사람이 있는 부분은 1, 배경은 0)
-in_features = model.roi_heads.box_predictor.cls_score.in_features 
-model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
 
 
@@ -104,27 +115,27 @@ model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
 # Option 2 : Modifying the model to add a different backbone
 
-import torchvision 
-from torchvision.models.detection import FasterRCNN 
-from torchvision.models.detection.rpn import AnchorGenerator 
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
 
-# Load a pre-trained model for classification and return only the features 
+# Load a pre-trained model for classification and return only the features
 # imagenet데이터로 pretrained된 mobilenet_v2모형을 로딩합니다.
 # 이 모형은 feature를 가공하는 부분만 있으며, 이 뒤에 classifier부분을 추가합니다.
-backbone = torchvision.models.mobilenet_v2(pretrained=True).features 
+backbone = torchvision.models.mobilenet_v2(pretrained=True).features
 
 # FasterRCNN needs to know the number of
 # output channels in a backbone. For Mobilenet_v2, It's 1280.
-# so we need to add it here. 
+# so we need to add it here.
 # 이 백본 모형의 최종 아웃풋 사이즈에 대한 정보를 지정해줍니다.
 backbone.out_channels = 1280
 
 # Let's make the RPN generate 5X3 anchors per spatial
 # location, with 5 different sizes and 3 different aspect
-# ratios. We have a Tuple because each feature 
+# ratios. We have a Tuple because each feature
 # map could potentially have different sizes and aspect ratios.
 
-anchor_generator = AnchorGenerator(sizes = ((32, 64, 128, 256, 512),), 
+anchor_generator = AnchorGenerator(sizes = ((32, 64, 128, 256, 512),),
                                    aspect_ratios = ((0.5, 1.0, 2.0),)
                                   )
 
@@ -132,11 +143,11 @@ anchor_generator = AnchorGenerator(sizes = ((32, 64, 128, 256, 512),),
 # use to perform the region of interest cropping, as well as
 # the size of the crop after rescaling.
 # If your backbone returns a Tensor, feature_names is expected to
-# bo [0]. More generally, the backbone should return an OrderedDict[Tensor], 
+# bo [0]. More generally, the backbone should return an OrderedDict[Tensor],
 # and in featuremap_names you can choose which feature maps to use.
 roi_pooler = torchvision.ops.MultiScaleRoIAlign(feature_names=[0],
                                                output_size=7,
-                                               sampling_ratio=2  
+                                               sampling_ratio=2
                                               )
 
 # put the pieces together inside a FasterRCNN model
@@ -149,29 +160,28 @@ model = FasterRCNN(backbone,
 
 # In our case, we want to fine-tune from a pre-trained model,
 # given that our dataset is very small, so we will be following
-# approach number 1 
+# approach number 1
 # 이 경우, 데이터셋이 매우 작기 때문에 pretrained 모형을 활용하는
 # 1번 방법으로 접근하도록 합니다.
 
 # Here we want to also compute the instance segmentation masks,
-# so we will be using Mask R-CNN 
+# so we will be using Mask R-CNN
 # instance segmentation mask를 활용, Mask R-CNN을 활용하도록 합니다.
 
-import torchvision 
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor 
-from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor 
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 def get_model_instance_segmentation(num_classes):
 
     model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-    in_features = model.roi_heads.box_predictor.cls_score.in_features 
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels 
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
     hidden_layer = 256
     model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
                                                        hidden_layer,
                                                        num_classes)
 
     return model
-
